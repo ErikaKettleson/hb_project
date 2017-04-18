0a2ccf65386f4a48f01bcff440bc783df5120874
Erika Kettleson <erikakettleson@gmail.com>
2017-04-17 19:39:31 -0700

    added kmeans algorithm incorperated into seed file

--- kmeans.py
+++ kmeans.py
@@ -2,7 +2,6 @@ import numpy as np
 import random
 from collections import namedtuple
 from math import sqrt
-import cv2
 from PIL import Image, ImageDraw, ImageColor, ImageFilter
 import requests
 from StringIO import StringIO
@@ -10,6 +9,67 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import argparse
 import utils
+from sqlalchemy import func
+from model import Show, Show_Color, Brand, Color, connect_to_db, db
+from server import app
+import os
+import sys
+from webcolors import *
+
+years = [2017]
+seasons = ['fall', 'spring']
+# brands = {"Carven": "/carven"}
+brands = {
+    "Acne Studios": "/acne-studios",
+    "Alexander McQueen": "/alexander-mcqueen",
+    "Alexander Wang": "/alexander-wang",
+    "Altuzarra": "/altuzarra",
+    "Ann Demeulemeester": "/ann-demeulemeester",
+    "Antonio Berardi": "/antonio-berardi",
+    "Balenciaga": "/balenciaga",
+    "Balmain": "/balmain",
+    "Bottega Veneta": "/bottega-veneta",
+    "Calvin Klein": "/calvin-klein",
+    "Carven": "/carven",
+    "Celine": "/celine",
+    "Chanel": "/chanel",
+    "Christian Dior": "/christian-dior",
+    "Christopher Kane": "/christopher-kane",
+    "Comme Des Garcons": "/comme-des-garcons",
+    "Derek Lam": "/derek-lam",
+    "Isabel Marant": "/isabel-marant",
+    "Dolce Gabbana": "/dolce-gabbana",
+    "Dries Van Noten": "/dries-van-noten",
+    "Etro": "/etro",
+    "Fendi": "/fendi",
+    "Giambattista Valli": "/giambattista-valli",
+    "Givenchy": "/givenchy",
+    "Gucci": "/gucci",
+    "Hermes": "/hermes",
+    "J.W. Anderson": "/j-w-anderson",
+    "Junya Watanabe": "/junya-watanabe",
+    "Kenzo": "/kenzo",
+    "Lanvin": "/lanvin",
+    "Loewe": "/loewe",
+    "Louis Vuitton": "/louis-vuitton",
+    "Maison Margiela": "/maison-martin-margiela",
+    "Marc Jacobs": "/marc-jacobs",
+    "Marni": "/marni",
+    "Mary Katrantzou": "/mary-katrantzou",
+    "Michael Kors": "/michael-kors-collection",
+    "Miu Miu": "/miu-miu",
+    "Missoni": "/missoni",
+    "Oscar de la Renta": "/oscar-de-la-renta",
+    "Prada": "/prada",
+    "Proenza Schouler": "/proenza-schouler",
+    "Roksanda": "/roksanda",
+    "Stella McCartney": "/stella-mccartney",
+    "Saint Laurent": "/saint-laurent",
+    "Tory Burch": "/tory-burch",
+    "Valentino": "/valentino",
+    "Vetements": "/vetements",
+}
+
 
 Point = namedtuple('Point', ('coords', 'n', 'ct'))
 Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
@@ -22,21 +82,23 @@ def get_points(img):
         points.append(Point(color, 3, count))
     return points
 
-rtoh = lambda rgb: '#%s' % ''.join(('%02x' % p for p in rgb))
 
-
-def colorz(img, n=5):
-    # response = requests.get(filename)
-    # img = Image.open(StringIO(response.content))
-    # img = Image.open(filename)
-    # img.thumbnail((450, 990))
+def colorz(img, n=4):
     w, h = img.size
 
     points = get_points(img)
     clusters = kmeans(points, n, 1)
     rgbs = [map(int, c.center.coords) for c in clusters]
     print "rgbs", rgbs
-    return map(rtoh, rgbs)
+    hex_codes = []
+    color_names = []
+    for rgb in rgbs:
+        hex_codes.append(rgb_to_hex(rgb))
+    print "hex codes", hex_codes
+    for hex_code in hex_codes:
+        color_names.append(hex_to_name(hex_codes))
+    print "color names", color_names
+    return rgbs
 # import ipdb; ipdb.set_trace()
 
 
@@ -63,7 +125,7 @@ def kmeans(points, k, min_diff):
         plists = [[] for i in range(k)]
 
         for p in points:
-            smallest_distance = float('Inf')
+            smallest_distance = float('Inf') # infinity!!!
             for i in range(k):
                 distance = euclidean(p, clusters[i].center)
                 if distance < smallest_distance:
@@ -82,8 +144,10 @@ def kmeans(points, k, min_diff):
         if diff < min_diff:
             break
 
-    print clusters
+    # print clusters
     return clusters
+
+# GOT TO THUMBNAIL THE CROP BEFORE PROCESSINGS
 
 
 def crop_image(img):
@@ -112,9 +176,14 @@ def crop_image(img):
 
     crop = img.crop(box)
     bg_crop = img.crop(bg_box)
-    LOE_crop = crop.save("loe_crop.jpg")
-    loe_bg_crop = bg_crop.save("loe_bg_crop.jpg")
-    colorz(crop)
+    # LOE_crop = crop.save("loe_crop.jpg")
+    # loe_bg_crop = bg_crop.save("loe_bg_crop.jpg")
+    basewidth = 200
+    wpercent = (basewidth/float(crop.size[0]))
+    hsize = int((float(crop.size[1])*float(wpercent)))
+    img = crop.resize((basewidth, hsize))
+
+    colorz(img)
 
 crop_image("http://assets.vogue.com/photos/58a110cd3decdb1c740dcc22/master/pass/_UMB2280.jpg")
 
